32356be44404843f65536adc0bb31421bdef887c
halldorholgersson <halldor1995@gmail.com>
2021-09-16 17:35:45 +0000

    entities bby

---
+++ TechnicalRadiation/TechnicalRadiation.Repositories/Data/NewDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace TechnicalRadiation.TechnicalRadiation.Repositories.Data
+{
+    public class NewDbContext: DbContext
+    {
+        public NewDbContext(DbContextOptions<NewDbContext> options) : base(options) { }
+    
+    }
+}
